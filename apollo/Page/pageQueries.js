e1fb885c867fc1766a5843ee141341cc66cb9b80
Michael Crook <michael.k.crook@gmail.com>
2017-06-28 19:28:36 +0800

    moved tests for dashCase

--- apollo/Page/pageQueries.js
+++ apollo/Page/pageQueries.js
@@ -19,32 +19,3 @@ export default (obj, args) => {
       });
   });
 };
-
-
-
-const result1 = dashCaseToCamelCase('fantastic'); // expect 'fantastic'
-const result2 = dashCaseToCamelCase('fant-astic'); // expect 'fantAstic'
-const result3 = dashCaseToCamelCase('-fantastic'); // expect 'fantastic'
-const result4 = dashCaseToCamelCase('--fantastic'); // expect 'fantastic'
-const result5 = dashCaseToCamelCase('fantastic-'); // expect 'fantastic'
-const result6 = dashCaseToCamelCase('fantastic--'); // expect 'fantastic'
-const result7 = dashCaseToCamelCase('fant--astic'); // expect 'fantAstic'
-const result8 = dashCaseToCamelCase('fant-a-stic'); // expect 'fantAstic'
-const result9 = dashCaseToCamelCase(''); // expect ''
-const result10 = dashCaseToCamelCase('-a'); // expect 'a'
-const result11 = dashCaseToCamelCase('-'); // expect ''
-const result12 = dashCaseToCamelCase('--'); // expect ''
-
-console.log(result1);
-console.log(result2);
-console.log(result3);
-console.log(result4);
-console.log(result5);
-console.log(result6);
-console.log(result7);
-console.log(result8);
-console.log(result9);
-console.log(result10);
-console.log(result11);
-console.log(result12);
-
--- apollo/utils/quertyTools.js
+++ apollo/utils/quertyTools.js
@@ -19,3 +19,33 @@ export const dashCaseToCamelCase = (dashCase, startingIndex = 0) => {
   return dashCase;
 };
 
+
+
+// move to tests
+/*
+
+const result1 = dashCaseToCamelCase('fantastic'); // expect 'fantastic'
+const result2 = dashCaseToCamelCase('fant-astic'); // expect 'fantAstic'
+const result3 = dashCaseToCamelCase('-fantastic'); // expect 'fantastic'
+const result4 = dashCaseToCamelCase('--fantastic'); // expect 'fantastic'
+const result5 = dashCaseToCamelCase('fantastic-'); // expect 'fantastic'
+const result6 = dashCaseToCamelCase('fantastic--'); // expect 'fantastic'
+const result7 = dashCaseToCamelCase('fant--astic'); // expect 'fantAstic'
+const result8 = dashCaseToCamelCase('fant-a-stic'); // expect 'fantAstic'
+const result9 = dashCaseToCamelCase(''); // expect ''
+const result10 = dashCaseToCamelCase('-a'); // expect 'a'
+const result11 = dashCaseToCamelCase('-'); // expect ''
+const result12 = dashCaseToCamelCase('--'); // expect ''
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+console.log(result6);
+console.log(result7);
+console.log(result8);
+console.log(result9);
+console.log(result10);
+console.log(result11);
+console.log(result12);
