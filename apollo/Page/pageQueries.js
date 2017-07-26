ef6c5c483bbca0ce09584b7ec0d73b7e1e7a7195
Michael Crook <michael.k.crook@gmail.com>
2017-07-26 12:43:26 +0800

    added commenting

--- apollo/Page/pageQueries.js
+++ apollo/Page/pageQueries.js
@@ -6,14 +6,24 @@ import { camelCaseYoastObject } from '../Yoast/util';
 /* ----------- WP REST API v2 endpoints ----------- */
 const wpPagesURL = `${WP_API}/wp/v2/pages`;
 
+/*
+  Given an optional splat, will find the page with the matching url.
+  this is done via getting all pages with the slug (last item inside
+  of splay seperated via / and then we find the link which matches our
+  splay from this result
+ */
 export default (obj, args) => {
   const { splat = '' } = args;
+  // if we have no slug, it's home so we need to add the Home Slug
   const slug = getSlug(splat) || HOME_SLUG;
+  // covert the splat into a full URL for matching (remove trailing /)
   const splatAsUrl = `${WP_URL}/${splat}`.replace(/\/$/, '');
   return new Promise((resolve, reject) => {
     const wpPageURL = `${wpPagesURL}?slug=${slug}`;
     return axios.get(wpPageURL)
       .then(res => {
+        // the above query returns 0->many pages, so we need
+        // to find the correct page via link matching
         const page = res.data.filter(slugPage => {
           const { link = '' } = slugPage;
           const linkWithoutSlash = link.replace(/\/$/, '');
@@ -24,7 +34,7 @@ export default (obj, args) => {
           page.yoast = camelCaseYoastObject(page.yoast);
           resolve(page);
         } else {
-          reject('page or yoast null');
+          reject('page not found');
         }
       })
       .catch(error => {
