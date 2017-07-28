affdb8461a00c05ea5bbfefc210bcbaaff44fc5e
Michael Crook <michael.k.crook@gmail.com>
2017-07-28 15:19:11 +0800

    migrated user query to proxy

--- apollo/User/index.js
+++ apollo/User/index.js
@@ -1,4 +1,23 @@
+/**
+ * Example usage:
+ * note: never place variables into queries directly
+ * use arguments: http://graphql.org/graphql-js/passing-arguments/
+ *
+ query UserQuery {
+  user(id:1) {
+    ...userFragment
+  }
+  users {
+  	...userFragment
+  }
+}
 
+ fragment userFragment on User {
+		id
+    name
+    avatar_urls
+}
+ */
 const User = `
 type User {
     id: ID!
--- apollo/User/userQueries.js
+++ apollo/User/userQueries.js
@@ -1,32 +1,11 @@
-import axios from 'axios';
-import { WP_API } from '../../config/app';
+import { createWordpressGraphqlProxy } from '../utils/queryTools';
 
-/* ----------- WP REST API v2 endpoints ----------- */
-const wpUserURL = `${WP_API}/wp/v2/users`;
+const wpUserProxy = createWordpressGraphqlProxy('wp/v2/users');
 
 export const getUsers = () => {
-  return new Promise((resolve, reject) => {
-    return axios.get(wpUserURL)
-      .then(res => {
-        const users = res.data;
-        resolve(users);
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
+  return wpUserProxy.selectAll();
 };
 
 export const getUser = (obj, { id }) => {
-  return new Promise((resolve, reject) => {
-    const getUserUrl = `${wpUserURL}/${id}`;
-    return axios.get(getUserUrl)
-      .then(res => {
-        const users = res.data;
-        resolve(users);
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
+  return wpUserProxy.select(id);
 };
