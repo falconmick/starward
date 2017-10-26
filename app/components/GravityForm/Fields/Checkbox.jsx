49a1cef18f1f60cde5ae2917bcf3219930b5349c
Michael Crook <michael.k.crook@gmail.com>
2017-10-26 18:04:10 +0800

    removed crud

--- app/components/GravityForm/Fields/Checkbox.jsx
+++ app/components/GravityForm/Fields/Checkbox.jsx
@@ -76,19 +76,6 @@ const CheckboxField = (props) => {
   );
 };
 
-
-/*
-<textarea
-  name={id}
-  type={type}
-  value={!value ? '' : value}
-  placeholder={placeholder}
-  maxLength={maxLength}
-  required={isRequired}
-  onChange={(event) => updateForm(onInputChange(id, event))}
-/>
-*/
-
 // all Fields now return a component to render and validation
 // to ensure that upon load we are able to correctly calculate
 // form state (which relies on validation that must be ran before
@@ -100,71 +87,3 @@ export default {
   component: CheckboxField,
   validation: fieldValidation
 };
-
-
-/*
-import React, { Component } from 'react';
-import { checkboxValidation } from '../Helpers/validation';
-
-export default class Checkbox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { values: []};
-  }
-  componentWillMount() {
-    this.initField({target: null}, this.props.field);
-  }
-  componentWillReceiveProps(nextProps) {
-    if (this.props.submitSuccess !== nextProps.submitSuccess) {
-      this.initField({target: null}, nextProps.field);
-    }
-  }
-  initField(event, field) {
-    const { id, choices, required } = field;
-    const defaultValues = choices.filter(choice => choice.isSelected);
-    const valid = checkboxValidation(required, defaultValues);
-    this.setState({values: defaultValues});
-    this.props.updateForm(defaultValues, id, valid);
-  }
-  updateField(event, field) {
-    const { id, required } = field;
-    const value = event.target.value;
-    const selectedValues = this.state.values;
-    const valueIndex = selectedValues.indexOf(value);
-    if (valueIndex < 0) {
-      selectedValues.push(event.target.value);
-    } else {
-      selectedValues.splice(valueIndex, 1);
-    }
-    const valid = checkboxValidation(required, selectedValues);
-    this.setState({values: selectedValues});
-    this.props.updateForm(selectedValues, id, valid);
-  }
-  render() {
-    const { field, value, submitFailed, isValid } = this.props;
-    const { choices, label, classes, required } = field;
-    const values = value ? value.map(val => val.value) : [];
-    return (
-      <div className={!isValid && submitFailed ? `field error ${classes}` : `field ${classes}`}>
-        <div className="checkboxes">
-          <p className="title">{label}{required ? <abbr>*</abbr> : null}</p>
-          {choices.map((choice, index) => (
-            <div className="checkbox" key={index}>
-              <label htmlFor={choice.id}>
-                <input
-                  type="checkbox"
-                  name={choice.id}
-                  value={choice.value}
-                  checked={values.indexOf(choice.value) !== -1}
-                  onClick={(event) => this.updateField(event, field)}
-                />
-                {choice.text}
-              </label>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
-*/
