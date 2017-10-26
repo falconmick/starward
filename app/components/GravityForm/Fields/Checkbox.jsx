1bb333c2fcd1b7aa626bf589bfeba81835ca731d
Michael Crook <michael.k.crook@gmail.com>
2017-10-26 14:12:45 +0800

    working on checkboxes

--- app/components/GravityForm/Fields/Checkbox.jsx
+++ app/components/GravityForm/Fields/Checkbox.jsx
@@ -1,3 +1,82 @@
+import React from 'react';
+import { checkboxValidation } from '../Helpers/validation';
+import { Field } from './Field';
+import { FormField } from './FormField';
+
+// form validation MUST follow the signature of
+// field (from graphql), value (current state)
+// as this function is used by the container component
+// in a generic way for all fields. Signature:
+// (field, value) => boolean
+const fieldValidation = (field, value) => {
+  const { isRequired } = field;
+  const fieldIsValid = checkboxValidation(isRequired, value);
+  return fieldIsValid;
+};
+
+// may be very simple, but more complicated
+// fields could have more complex requriements
+// so this is here as an example of how to do so.
+const updateField = (id, value) => {
+  return {
+    value,
+    id
+  };
+};
+
+// extract the input and create a payload to send
+// to the updateForm callback
+const onInputChange = (id, event) => {
+  const value = event.target ? event.target.value : null;
+  return updateField(id, value);
+};
+
+const CheckboxField = (props) => {
+  const { field, fieldState, isValid, updateForm } = props;
+  const { value } = fieldState;
+  const { id, type, label, cssClass, placeholder, isRequired, maxLength } = field;
+  return (
+    <FormField cssClass={cssClass} isValid={isValid} fieldType="textarea">
+      <Field htmlFor={id} label={label} isRequired={isRequired}>
+        {/*<input*/}
+          {/*type="checkbox"*/}
+          {/*name={choice.id}*/}
+          {/*value={choice.value}*/}
+          {/*checked={values.indexOf(choice.value) !== -1}*/}
+          {/*onClick={(event) => this.updateField(event, field)}*/}
+        {/*/>*/}
+      </Field>
+    </FormField>
+  );
+};
+
+
+/*
+<textarea
+  name={id}
+  type={type}
+  value={!value ? '' : value}
+  placeholder={placeholder}
+  maxLength={maxLength}
+  required={isRequired}
+  onChange={(event) => updateForm(onInputChange(id, event))}
+/>
+*/
+
+// all Fields now return a component to render and validation
+// to ensure that upon load we are able to correctly calculate
+// form state (which relies on validation that must be ran before
+// we construct the DOM, which means that we cannot wait for the
+// Component to be mounted before we have access to it's validation
+// methods.. The other benifit of extracting validation is the consuming
+// component can choose to ignore validation if it pleases.
+export default {
+  component: CheckboxField,
+  validation: fieldValidation
+};
+
+
+/*
 import React, { Component } from 'react';
 import { checkboxValidation } from '../Helpers/validation';
 
@@ -62,3 +141,4 @@ export default class Checkbox extends Component {
     );
   }
 }
+*/
