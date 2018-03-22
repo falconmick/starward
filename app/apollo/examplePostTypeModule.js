3c1dc0e1a344330dce0589787399572c94906de6
Michael Crook <michael.k.crook@gmail.com>
2018-03-23 07:08:39 +1100

    improved example doco on pst types

--- app/apollo/examplePostTypeModule.js
+++ app/apollo/examplePostTypeModule.js
@@ -10,6 +10,13 @@ const { bundle: genreBundle, rootQuery: genreRootQuery, addToPostTypeArgs: addGe
 // note: you can create a custom ACF flexible content by copying apollo/types/acf
 // from there pass in it's Type and Type name via the acf named argument of postTypeFactory
 // it's on my todo: list to make an ACF factory. From there it would return a bundle and a addAcfArgs like taxonomyFactory
+// after copying the Acf folder mentioned above, you would mofidy FlexibleContent/index.js to return a different queryable
+// as this lets us define new prefetch cache resolvers (i.e. FormSection)
+//
+// NOTE: you don't have to send down a custom ACF if your not using the default page layout, you just wont be able
+// to define the prefetch cache resolvers mentioned above. So if all of your data exists in the Fields you send down
+// and you dont need to do a second API request based on what the user placed into the field then don't bother making a
+// custom acf type. If in doubt message michael@birdbrain.com.au
 const { bundle: bookBundle, rootQuery: bookRootQuery } = postTypeFactory({typeName: 'Book', apiEndpoint: 'books-api', taxonomies: [addGenresArgs]});
 
 const rootQuery = joinQueryDefinitionString([genreRootQuery, bookRootQuery]);
