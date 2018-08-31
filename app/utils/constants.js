3460a369e80223a7ac3b096baef7fdc53120b1fa
Michael Crook <michael.k.crook@gmail.com>
2018-08-31 17:14:28 +0800

    added place to put things like WP thumbs

---
+++ app/utils/constants.js
@@ -0,0 +1,9 @@
+// inbuilt thumbnails
+export const WP_SIZE_LARGE = 'large';
+export const WP_SIZE_MEDIUM = 'medium';
+export const WP_SIZE_MEDIUM_LARGE = 'medium_large';
+export const WP_SIZE_THUMBNAIL = 'thumbnail';
+
+// example custom thumbnails
+export const WP_EXTRA_WIDE = 'extra_wide';
+export const WP_SIZE_GALLERY = 'gallery';
